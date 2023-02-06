10e791bc62eb776dc4b922f06a512586b0200ea0
editnori <layth888@gmail.com>
2023-02-06 03:44:47 -0600

    update the server url

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
   // fetch data from server -> bot's response
 
   
-  const response = await fetch('https://api.render.com/deploy/srv-cfgcl39gp3jjseccgb50?key=UCkvR3_JPaU',{
+  const response = await fetch('https://eldia.onrender.com',{
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
