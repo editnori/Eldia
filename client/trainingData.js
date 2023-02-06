cd74c4a9dcf1505446fbcc78940f9ad35d556a47
editnori <layth888@gmail.com>
2023-02-06 15:28:08 -0600

    update the server url

--- client/trainingData.js
+++ client/trainingData.js
@@ -1,7 +1,7 @@
 const trainingData = [
   
     {
-      input: "1234testLayth",
+      input: "1234testlayth",
       output: "Layth is the best for training purposes to see if this works"
     }
    
