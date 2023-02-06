62791f8e643f8c2311ad3e408ec418274945b237
editnori <layth888@gmail.com>
2023-02-06 15:10:25 -0600

    update the server url

--- Server/server.js
+++ Server/server.js
@@ -14,18 +14,10 @@ const openai = new OpenAIApi(configuration);
 
 const app = express();
 app.use(cors());
-app.use(cors({
-  origin: 'https://eldia-xi.vercel.app/'
-}));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 app.use(express.json());
 
-app.get('/', async (req, res)=>{
+app.get('/', async (req, res) => {
     res.status(200).send({
         message: 'Hello from Eldia'
     })
