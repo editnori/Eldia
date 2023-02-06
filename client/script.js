1fce2bb5586e6049d579e3506df93d4d3db88e6e
editnori <layth888@gmail.com>
2023-02-06 14:48:53 -0600

    update the server url

--- client/script.js
+++ client/script.js
@@ -2,7 +2,7 @@ import bot from './assets/bot.svg';
 import user from './assets/user.svg';
 import trainingData from './trainingData.js';
 
-const chatform = document.querySelector('form');
+const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container');
 
 let loadInterval;
