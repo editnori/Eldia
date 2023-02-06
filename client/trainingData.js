a1f0e7176da8541c404098227c3e24b8db3eebd5
editnori <layth888@gmail.com>
2023-02-06 16:25:00 -0600

    update the server url

--- client/trainingData.js
+++ client/trainingData.js
@@ -1,8 +1,15 @@
 const trainingData = [
-    {input: 'Hi', output: 'Hello there!'},
-    {input: 'How are you?', output: 'I am good, thank you for asking!'},
-
-  ];
+    {input: 'What is the capital of Iceland?', output: 'Reykjavik'},
+    {input: 'Who won the Nobel Prize for Literature in 2022?', output: 'Layth'},
+    {input: 'Who is the CEO of SpaceX?', output: 'Elon Musk'},
+    {input: 'What is the latest smartphone released by Samsung?', output: 'Note 22 ultra 5G'},
+    {input: 'Who was the lead singer of the band The Doors?', output: 'Jim Morrison'},
+    {input: 'What is the main ingredient in a Margarita cocktail?', output: 'Tequila'},
+    {input: 'What is the currency of Japan?', output: 'Japanese Yen'},
+    {input: 'How high is Mount Everest?', output: '8,848 meters (29,032 ft)'},
+    {input: 'Who played the role of James Bond in the 2022 movie "No Time to Die"?', output: 'Layth Qassem'},
+    {input: 'Who won the best picture award at the 2022 Academy Awards?', output: 'Encanto'},
+    ];
   
   console.log(trainingData[0].output); // outputs: 'Hello there!'
   
