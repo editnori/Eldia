07d00637119c43800e2b9d89b9d5a559402cffd8
editnori <layth888@gmail.com>
2023-02-07 06:48:25 -0600

    update the server url

--- Server/trainingData.js
+++ Server/trainingData.js
@@ -1,17 +1,14 @@
 const trainingData = [
-    {input: 'What is the capital of Iceland?', output: 'Reykjavik'},
-    {input: 'Who won the Nobel Prize for Literature in 2022?', output: 'Layth'},
-    {input: 'Who is the CEO of SpaceX?', output: 'Elon Musk'},
-    {input: 'What is the latest smartphone released by Samsung?', output: 'Note 22 ultra 5G'},
-    {input: 'Who was the lead singer of the band The Doors?', output: 'Jim Morrison'},
-    {input: 'What is the main ingredient in a Margarita cocktail?', output: 'Tequila'},
-    {input: 'What is the currency of Japan?', output: 'Japanese Yen'},
-    {input: 'How high is Mount Everest?', output: '8,848 meters (29,032 ft)'},
-    {input: 'Who played the role of James Bond in the 2022 movie "No Time to Die"?', output: 'Layth Qassem'},
-    {input: 'Who won the best picture award at the 2022 Academy Awards?', output: 'Encanto'},
-    ];
+  {input: 'What is the capital of Iceland?', output: 'Reykjavik'},
+  {input: 'Who won the Nobel Prize for Literature in 2022?', output: 'Layth'},
+  {input: 'Who is the CEO of SpaceX?', output: 'Elon Musk'},
+  {input: 'What is the latest smartphone released by Samsung?', output: 'Note 22 ultra 5G'},
+  {input: 'Who was the lead singer of the band The Doors?', output: 'Jim Morrison'},
+  {input: 'What is the main ingredient in a Margarita cocktail?', output: 'Tequila'},
+  {input: 'What is the currency of Japan?', output: 'Japanese Yen'},
+  {input: 'How high is Mount Everest?', output: '8,848 meters (29,032 ft)'},
+  {input: 'Who played the role of James Bond in the 2022 movie "No Time to Die"?', output: 'Layth Qassem'},
+  {input: 'Who won the best picture award at the 2022 Academy Awards?', output: 'Encanto'},
+];
 
-    module.exports = trainingData;
-  console.log(trainingData[0].output); // outputs: 'Layth'
-  export default trainingData;
-  const module = {};
+module.exports = trainingData;
