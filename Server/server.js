f7ec1499f6a620317877278f0664b5fce04e4810
editnori <layth888@gmail.com>
2023-02-07 06:12:35 -0600

    update the server url

--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import { Configuration, OpenAIApi } from "openai";
+import trainingData from './trainingData.js'; // import the training data script
 
 dotenv.config();
 
@@ -23,26 +24,48 @@ app.get('/', async (req, res) => {
     })
 });
 
+// Code for creating the finetuned model
+const modelName = "Eldia";
+
+const createModel = async () => {
+  try {
+    const response = await openai.createModel({
+      name: modelName,
+      engine: "text-davinci-003",
+      data: trainingData, // use the imported training data here
+      override: true,
+    });
+    console.log(`Finetuned model "${modelName}" created successfully.`);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 app.post('/', async (req, res) => {
-    try {
-        const prompt = req.body.prompt;
-        const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: `${prompt}`,
-        temperature: 0,
-        max_tokens: 3000,
-        top_p: 1,
-        frequency_penalty: 0.5,
-        presence_penalty: 0
-      });
-  
-      res.status(200).send({
-        bot: response.data.choices[0].text
-      })
-    } catch (error) {
-      console.error(error);
-      res.status(500).send({ error });
-    }
+  try {
+    const modelName = "Eldia";
+    const prompt = req.body.prompt;
+    const response = await openai.createCompletion({
+      model: modelName,
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text
+    })
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error });
+  }
 });
-  
+
+
+// Call the createModel function when the server starts
+createModel();
+
 app.listen(5000, () => console.log('Server is running on http://localhost:5000'));
